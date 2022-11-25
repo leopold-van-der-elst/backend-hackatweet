5bac448b1f794283dd4be900d2c543ceae023727
Lumpa12 <damien.thevenin@gate-16.com>
2022-11-25 17:18:22 +0100

    ?

--- routes/hashtags.js
+++ routes/hashtags.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var router = express.Router();
-<<<<<<< HEAD
-=======
 const Tweet = require ("../models/tweets")
->>>>>>> ab093b4f635b7bd99fe01666d23b9e0de64004f3
 
 router.get('/:hashtag', (req, res) => {
     Tweet.findOne({ hashtag: req.params.hashtag }).then(data => {
@@ -14,11 +11,8 @@ router.get('/:hashtag', (req, res) => {
       }
     });
   });
-<<<<<<< HEAD
 
   module.exports = router;
   
-=======
   
-  module.exports = router;
->>>>>>> ab093b4f635b7bd99fe01666d23b9e0de64004f3
+
